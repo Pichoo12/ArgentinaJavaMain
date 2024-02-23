9a0e2a03c1b76b5eccba87a158c7f15a3cbb65ed
Pichoo12 <s1802049@online.houstonisd.org>
2024-02-23 11:46:26 -0600

    init

--- app/src/main/java/com/example/argentinajava/MainActivity.java
+++ app/src/main/java/com/example/argentinajava/MainActivity.java
@@ -1,22 +1,29 @@
 package com.example.argentinajava;
+
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
 public class MainActivity extends AppCompatActivity {
     private TextView timerTextView;
     private Button startStopButton;
+    private Button clearButton;
     private CountDownTimer countDownTimer;
     private boolean Running = false;
-    private long timeLeftInMillis = 300000;
+    private long timeLeftInMillis = 600000;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         timerTextView = findViewById(R.id.timerTextView);
         startStopButton = findViewById(R.id.startStopButton);
+        clearButton = findViewById(R.id.clearButton);
+
         startStopButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -27,8 +34,17 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetTimer();
+            }
+        });
+
         updateTimerText();
     }
+
     private void startTimer() {
         countDownTimer = new CountDownTimer(timeLeftInMillis, 1000) {
             @Override
@@ -36,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
                 timeLeftInMillis = millisUntilFinished;
                 updateTimerText();
             }
+
             @Override
             public void onFinish() {
                 Running = false;
@@ -45,25 +62,25 @@ public class MainActivity extends AppCompatActivity {
             }
         }.start();
         Running = true;
-        //when pasued 
         startStopButton.setText("Paused");
     }
+
     private void stopTimer() {
         countDownTimer.cancel();
         Running = false;
         startStopButton.setText("Start");
     }
-    //changes text on screen
+
+    private void resetTimer() {
+        stopTimer(); // Stop the timer if running
+        timeLeftInMillis = 600000; // Reset timer to 5 minutes
+        updateTimerText();
+    }
+
     private void updateTimerText() {
         int minutes = (int) (timeLeftInMillis / 1000) / 60;
         int seconds = (int) (timeLeftInMillis / 1000) % 60;
-
         String timeFormatted = String.format("%02d:%02d", minutes, seconds);
         timerTextView.setText(timeFormatted);
     }
 }
-
-
-
-
-
